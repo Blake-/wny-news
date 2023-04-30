169508f6acc1de10237c2a85a44467dbff2f32e8
Blake Carver <btcarver@lisnews.com>
2023-04-30 10:46:36 -0400

    fix that warning

--- news/urls.py
+++ news/urls.py
@@ -4,5 +4,5 @@ from django.views.generic.base import TemplateView
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
 ]
