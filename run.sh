585ecc640b5624a80d402bdf67cc6f4f0a3bf5e3
Blake Carver <btcarver@lisnews.com>
2023-04-30 10:22:53 -0400

    news at:

--- run.sh
+++ run.sh
@@ -2,12 +2,17 @@
 
 cd /Users/blake/Desktop/newsaggregator
 
-nohup python3 manage.py runserver --noreload &
+#nohup python3 manage.py runserver --noreload &
 
-sleep 15
+#sleep 15
 
-wget -m -k -p -nH -P wnynews.com  http://127.0.0.1:8000/
+#wget -m -k -p -nH -P wnynews.com  http://127.0.0.1:8000/
 
-echo "Got it!"
+#echo "Got it!"
 
-pkill -f runserver
+#pkill -f runserver
+
+time=$(date +%r)
+git add .
+git commit -m "news at: $timestamp"
+git push origin main
