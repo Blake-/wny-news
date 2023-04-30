aff29fac1beef35d9a596a939bd67f7e8ad2fe8c
Blake Carver <btcarver@lisnews.com>
2023-04-30 10:28:07 -0400

    full paths

--- run.sh
+++ run.sh
@@ -2,17 +2,15 @@
 
 cd /Users/blake/Desktop/newsaggregator
 
-nohup python3 manage.py runserver --noreload &
+/usr/bin/nohup /opt/homebrew/bin/python3 manage.py runserver --noreload &
 
-sleep 25
+/bin/sleep 25
 
-wget -m -k -p -nH -P wnynews.com  http://127.0.0.1:8000/
+/opt/homebrew/bin/wget -m -k -p -nH -P wnynews.com  http://127.0.0.1:8000/
 
-#echo "Got it!"
-
-pkill -f runserver
+/usr/bin/pkill pkill -f runserver
 
 time=$(date +%c)
-git add .
-git commit -m "News update: $time"
-git push origin main
+/usr/bin/git add .
+/usr/bin/git commit -m "News update: $time"
+/usr/bin/git push origin main
